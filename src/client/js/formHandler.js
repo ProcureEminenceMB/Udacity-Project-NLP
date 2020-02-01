1d15df2c8a06d01a10c85f79a8d8eea02823820c
Michael Bigelow <procureeminence@gmail.com>
2020-02-01 12:34:03 -0500

    JS Code Cleanup

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,19 @@
-function handleSubmit(event) {
-    event.preventDefault()
+function handleSubmit( event ) {
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+	event.preventDefault();
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+	// check what text was put into the form field
+	let formText = document.getElementById( 'name' ).value;
+	checkForName( formText );
+
+	console.log( '::: Form Submitted :::' );
+	
+	fetch( 'http://localhost:8080/test' )
+		.then( res => res.json() )
+		.then( function( res ) {
+			document.getElementById( 'results' ).innerHTML = res.message;
+		});
+
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,16 +1,21 @@
-function checkForName(inputText) {
-    console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
+function checkForName( inputText ) {
 
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
-    }
+	console.log( '::: Running checkForName :::', inputText );
+
+	let names = [
+		'Picard',
+		'Janeway',
+		'Kirk',
+		'Archer',
+		'Georgiou'
+	];
+
+	if( names.includes( inputText ) ) {
+
+		alert("Welcome, Captain!");
+
+	}
+
 }
 
-export { checkForName }
+export { checkForName };
