4a12ff8a4d6b9bbf7762a0df45c6f62e0cce5bf5
Michael Bigelow <procureeminence@gmail.com>
2020-02-01 12:11:01 -0500

    Added dotenv & Aylien Text API Setup Code

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,22 @@
+const dotenv = require( 'dotenv' );
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+let aylien = require( 'aylien_textapi' );
 
 const app = express()
 
 app.use(express.static('dist'))
+
+let textapi = new aylien({
+	application_id: process.env.API_ID,
+	application_key: process.env.API_KEY
+});
+
+console.log( `API ID: ${process.env.API_ID}` );
+console.log( `API KEY: ${process.env.API_KEY}` );
 
 console.log(__dirname)
 
