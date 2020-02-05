978e61797eba6a7f5cbc78440078a2097972562c
Michael Bigelow <procureeminence@gmail.com>
2020-02-04 21:37:23 -0500

    Adding Detailed Comments

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,6 @@
 import { validURL } from './inputValidation';
 
+// Define function that processes the form submission
 const handleSubmit = ( event ) => {
 
 	event.preventDefault(); // Prevent page refresh caused by form submission
@@ -7,6 +8,7 @@ const handleSubmit = ( event ) => {
 	// Get form data
 	let formText = ( document.getElementById( 'URL' ) ) ? document.getElementById( 'URL' ).value: "";
 
+	// Exit function early if the URL is invalid
 	if( !validURL( formText ) ){
 
 		alert( "Please enter a valid, full URL address.\nExample: https://www.google.com/" );
@@ -14,8 +16,10 @@ const handleSubmit = ( event ) => {
 
 	}
 
+	// Update 'Results' element to let the user know the URL is being processed
 	document.getElementById( 'Results' ).innerHTML = 'Processing URL...';
 	
+	// Send URL request to the server and wait for Aylien API response
 	fetch( '/process', {
 		method: 'POST',
 		credentials: 'same-origin',
@@ -26,9 +30,11 @@ const handleSubmit = ( event ) => {
 			'url': formText
 		} )
 	})
+	// Convert response to JSON format
 	.then( (response) => {
 		return response.json();
 	} )
+	// Display results to the user
 	.then( (data) => {
 		
 		let resultText = "";
--- src/client/js/inputValidation.js
+++ src/client/js/inputValidation.js
@@ -1,15 +1,16 @@
+// Define function that determines if URL is valid
 const validURL = ( url ) => {
 
 	let valid = false;
 
 	try{
 
-		let urlObj = new URL( url );
+		let urlObj = new URL( url ); // URL is valid if constructor doesn't throw TypeError exception
 		valid = true;
 
 	}catch( error ){
 
-		console.log( error.message );
+		console.log( error.message ); // Log URL constructor TypeError to console
 
 	}
 
