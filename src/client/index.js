7ca7818ccba494325118eac466cf8d4dcc6fbbcb
Michael Bigelow <procureeminence@gmail.com>
2020-02-04 21:55:39 -0500

    Fixed SCSS File Order
    Certain css properties were being overwritten due to to the order of the file imports.

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,11 @@
 import { validURL } from './js/inputValidation';
 import { handleSubmit } from './js/formHandler';
 
-import './styles/base.scss';
-import './styles/footer.scss';
-import './styles/form.scss';
-import './styles/header.scss';
 import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/form.scss';
+import './styles/footer.scss';
 
 export {
 	validURL,
